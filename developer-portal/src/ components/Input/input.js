a87ec8d041174ae01bd475a4dd84a21896ad988b
Mary West <westmarye1219@gmail.com>
2018-12-31 17:50:30 -0600

    github works/auth works

---
+++ developer-portal/src/ components/Input/input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './input.scss';
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div className="Input">
+        <h2>Input</h2>
+      </div>
+    );
+  }
+}
+
+export default Input;
--- developer-portal/src/App/App.js
+++ developer-portal/src/App/App.js
@@ -1,37 +1,66 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import logo from './logo.svg';
-import './App.scss';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
+import connection from '../helpers/data/connections';
+
+import Auth from '../ components/Auth/auth';
+import Bio from '../ components/Bio/bio';
+import Assignments from '../ components/Assignments/assignments';
+import MyNavbar from '../ components/MyNavbar/MyNavbar';
+import Input from '../ components/Input/input';
+
+import './App.scss';
+import authRequests from '../helpers/data/authRequests';
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    connection();
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      this.setState({ authed: false });
+    };
+
+    if (!this.state.authed) {
+      return (
+        <div className="App">
+          <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
+          <Auth isAuthenticated={this.isAuthenticated}/>
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button className='btn btn-danger'>HELP ME</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Button
-    tag="a"
-    color="success"
-    size="large"
-    href="http://reactstrap.github.io"
-    target="_blank"
->
-    View Reactstrap Docs
-</Button>
-        </header>
+        <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
+        <Bio />
+        <Assignments />
+        <Input />
       </div>
     );
   }
